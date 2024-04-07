912b285e594a425cc46c78ca281db3bc9fc19733
Cyrilloche <che.cyril@hotmail.fr>
2024-04-07 13:13:40 +0200

    Implement Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
 import PokemonList from "./pages/pokemon-list";
+import PokemonsDetail from "./pages/pokemon-detail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App: FunctionComponent = () => {
 
     return (
-        <PokemonList />
+        <Router>
+            <div>
+                {/* La barre de navigation commune à toutes les pages */}
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <Link to="/" className="brand-logo center">Pokédex</Link>
+                    </div>
+                </nav>
+                {/* La système de gestion des routes de notre application */}
+                <Switch>
+                    <Route exact path="/" component={PokemonList} />
+                    <Route exact path="/pokemons" component={PokemonList} />
+                    <Route path="/pokemons/:id" component={PokemonsDetail} />
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
